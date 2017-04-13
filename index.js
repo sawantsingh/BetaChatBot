6c70ced9d14614ce39572a771a95a1da8ef2755a
Sawant kumar <sawakumar@deloitte.com>
2017-04-13 12:53:36 +0530

    reset to older commit

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 //This is still work in progress
 /*
-Please report any bugs to nicomwaks@gmail.com
-
-i have added console.log on line 48 
-
 
  */
 'use strict'
@@ -40,7 +36,6 @@ app.post('/tryone/', function (req, res) {
 	
 	console.log(req)
 
-
 	let urlString = 'https://api.darksky.net/forecast/5fa39d2d3870ab45753d970a62fb4777/' + req.body.coordinate
 
 	console.log(urlString)
@@ -54,10 +49,15 @@ app.post('/tryone/', function (req, res) {
 		} else if (response.body.error) {
 			console.log('Error: ', response.body.error)
 		}
-		res.send(response.body)
 
+		var json = JSON.parse(response.body);
+		console.log('Printing json:',json)
+
+		let lattitude = json["latitude"]
+		console.log('Printing json:',json)
 	})
 
+	res.sendStatus(200)
 })
 
 
@@ -73,17 +73,36 @@ app.post('/webhook/', function (req, res) {
 	let messaging_events = req.body.entry[0].messaging
 	for (let i = 0; i < messaging_events.length; i++) {
 		let event = req.body.entry[0].messaging[i]
-		//let sender = event.sender.id
+		let sender = event.sender.id
 		if (event.message && event.message.text) {
 			let text = event.message.text
 			if (text === 'Generic'){ 
-				//console.log("welcome to chatbot")
+				console.log("welcome to chatbot")
 				sendGenericMessage(sender)
 				continue
 			}
-			console.log("Test message substring" + text.substring(0,200));
+			if (text === 'weather'){
+				let urlString = 'https://api.darksky.net/forecast/5fa39d2d3870ab45753d970a62fb4777/37.8267,-122.4233' //req.body.coordinate
+				
+				request({
+					url: urlString,
+					method: 'GET'
+				}, function(error, response, body) {
+					if (error) {
+						console.log('Error sending messages: ', error)
+					} else if (response.body.error) {
+						console.log('Error: ', response.body.error)
+					}
+					if (response) {
+				 		// sendTextMessage(sender, "Received response, echo: " + text.substring(0, 200))
 
-			//sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+					}
+					sendResponseData(sender,response)
+				})
+				continue
+			}
+      
+			sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
 		}
 		if (event.postback) {
 			let text = JSON.stringify(event.postback)
@@ -98,6 +117,37 @@ app.post('/webhook/', function (req, res) {
 // recommended to inject access tokens as environmental variables, e.g.
 // const token = process.env.FB_PAGE_ACCESS_TOKEN
 const token = "EAAJbdXC4E9kBADgY9iNK3f8IRusZBNw1TfzvWJKAgbEtEBQBIyWnfa88agtHvFiCAL82airqv3o2YAC1ZC8p9woAxd2bv3awR5e5lcyRd7J4al3qCHPzAqDfYSvJXmDeCn1NN3ZAZAjwZBqg5uMq3K5AoVcGmognHKbWgFxWiSZBRHwKI5njXF"
+
+function sendResponseData(sender,response) {
+
+	 var json = JSON.parse(response.body);
+		console.log('Printing json:',json)
+
+		let hourly = json["hourly"]
+		let summary = hourly["summary"]
+		console.log('Printing json:',summary)
+
+
+	let messageData = { text:summary }
+
+
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {access_token:token},
+		method: 'POST',
+		json: {
+			recipient: {id:sender},
+			message: messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error)
+		}
+	})
+}
+
 
 function sendTextMessage(sender, text) {
 	let messageData = { text:text }
@@ -128,11 +178,11 @@ function sendGenericMessage(sender) {
 				"elements": [{
 					"title": "First card",
 					"subtitle": "Element #1 of an hscroll",
-					"image_url": "http://www.qspiders.com/sites/default/files/sunny%20amar%20nath.jpg",
+					"image_url": "https://scontent-sin6-1.xx.fbcdn.net/v/t31.0-8/17218524_10211278521054605_3920462141316408947_o.jpg?oh=8307dfaf91be1b3608dc99327f12fecb&oe=59971EED",
 					"buttons": [{
 						"type": "web_url",
-						"url": "https://www.messenger.com",
-						"title": "web url"
+						"url": "https://en.wikipedia.org/wiki/Deloitte",
+						"title": "Deloitte"
 					}, {
 						"type": "postback",
 						"title": "Postback",
@@ -141,8 +191,12 @@ function sendGenericMessage(sender) {
 				}, {
 					"title": "Second card",
 					"subtitle": "Element #2 of an hscroll",
-					"image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+					"image_url": "http://www.qspiders.com/sites/default/files/sunny%20amar%20nath.jpg",
 					"buttons": [{
+						"type": "web_url",
+						"url": "https://www.allianz.com/en/",
+						"title": "Allianz"
+					},{
 						"type": "postback",
 						"title": "Postback",
 						"payload": "Payload for second element in a generic bubble",
